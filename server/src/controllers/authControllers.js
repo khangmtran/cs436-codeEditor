3f030932de69c157b8d5e1849ac40d7c88c9ffdf
Adler Nguyen <79223596+anguyener03@users.noreply.github.com>
2024-10-08 12:01:52 -0700

    JWT
    Implemented JWT for logging in and signing up

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -1,19 +1,33 @@
 const User = require('../models/user.js')
+const jwt = require('jsonwebtoken')
 
+const createToken = (_id) =>{
+    return jwt.sign({_id},"adlerkhangfemorjaydencsc",{expiresIn: '3d'})
+}
 // POST create a user
 const createUser =  async (req, res) => {
     const {fname, lname, email, password} = req.body
     try{
         const user = await User.signup(fname,lname,email,password)
+        const token = createToken(user._id)
         res.status(200)
-        res.json({email,user})
+        res.json({email,token})
     }
     catch(error){
         res.status(400).json({error : error.message})
     }
 }
 const loginUser = async (req, res) =>{
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+    try{
+        const user = await User.login(email,password)
+        const token = createToken(user._id)
+        res.status(200)
+        res.json({email,token})
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 // login user
 
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt'); 
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -33,6 +34,18 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.signup = async function (firstName, lastName, email, password) {
+  
+  if(!email || !password || !firstName || !lastName){
+    throw Error("All fields must be filled out")
+  }
+  if(!validator.isEmail(email)){
+    throw Error("Enter is not valid")
+  }
+  if(!validator.isStrongPassword(password)){
+    throw Error("Password is not valid")
+  }
+  
+  
   const exist = await this.findOne({ email });
   
   if (exist) {
@@ -51,6 +64,20 @@ userSchema.statics.signup = async function (firstName, lastName, email, password
 
   return user;
 };
+userSchema.statics.login = async function (email, password){
+  if(!email || !password){
+    throw Error("All fields must be filled out")
+  }
+  const user = await this.findOne({email})
+  if(!user){
+    throw Error("Email does not exist");
+  }
+  const match = await bcrypt.compare(password, user.password)
+  if(!match){
+    throw Error("Incorrect password")
+  }
+  return user
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
