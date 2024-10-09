b28b31b470245c6f5fb6ea8daf3c00669420bd90
Adler Nguyen <adler.tng@gmail.com>
2024-10-09 01:25:18 -0700

    websockets

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Collaborative Code Editor  
-## Getting Started  
-cd to the folder, npm install to install dependencies, npm run dev to start dev server  
+# Collaborative Code Editor
+
+## Getting Started
+
+cd to the folder, npm install to install dependencies, npm run dev to start dev server
+
+Cd to server, npm install, npm start to run backend server,
--- server/server.js
+++ server/server.js
@@ -1,23 +1,47 @@
 const express = require('express')
+const WebSocket = require('ws');
+const { setupWSConnection } = require('y-websocket/bin/utils');
+const cors = require('cors');
+const http = require('http'); // Import the http module
+
+//Routes
 const authRoutes = require('./src/routes/authRoutes.js')
+
 const app = express()
 
 const mongoose = require('mongoose')
 
-
+app.use(cors())
 app.use(express.json())
+// log to the server any requests
 app.use((req,res,next) =>{
     console.log(req.path,req.params)
     next()
 })
+//mount routes
 app.use('/api/authRoutes',authRoutes)
 
+// Placeholder Code for running, will be replaces with a code Routes or something
+app.post('/run', async (req, res) => {
+    res.json({ output: 'Hello from the code execution endpoint!' });
+  });
+
+
+const server = http.createServer(app);
+
+// Create a WebSocket server attached to the HTTP server
+const wss = new WebSocket.Server({ server });
+ wss.on('connection', (ws, req) => {
+  setupWSConnection(ws, req);
+});
+console.log('conneceted to wss')
+// Connect to MongoDB and start the server
 mongoose.connect("mongodb+srv://root:R5O4lPtZsjhGczBC@collabcodeeditor.z5wf5.mongodb.net/")
-.then(()=>{
-    app.listen(4000, () =>{
-        console.log("Listening on port 4000")
-    })
-})
-.catch((error)=>{
-    console.log(error)
-})
+  .then(() => {
+    server.listen(4000, () => {
+      console.log('HTTP and WebSocket server running on http://localhost:4000');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
